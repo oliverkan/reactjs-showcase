89f11b9a2aabe5ccfb06fdd4c892140f89528b7c
oliverkan <erkancaglartaskin@gmail.com>
2020-06-02 01:28:41 +0100

    login screen and api calls added

---
+++ src/services/authService.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+const API_URL = process.env.REACT_APP_API_ADDRESS;
+class AuthService {
+    login(username, password) {
+        console.log(API_URL)
+        return axios
+            .post(API_URL + '/auth/login', {
+                userName:username,
+                password
+            })
+            .then(response => {
+                if (response.data.accessToken) {
+                    localStorage.setItem("user", JSON.stringify(response.data));
+                }
+
+                return response.data;
+            });
+    }
+
+    logout() {
+        localStorage.removeItem("user");
+    }
+
+    register(username, email, password) {
+        return axios.post(API_URL + "/auth/signup", {
+            username,
+            email,
+            password
+        });
+    }
+
+    getCurrentUser() {
+        return JSON.parse(localStorage.getItem('user'));;
+    }
+}
+
+export default new AuthService();
