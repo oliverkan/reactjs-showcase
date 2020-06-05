8a4b4508853675be0ffeff27c00737e9f21ce058
oliverkan <erkancaglartaskin@gmail.com>
2020-06-05 15:54:45 +0100

    datatable added to list registered users on user page

--- src/views/user.js
+++ src/views/user.js
@@ -1,21 +1,22 @@
 import React, {Component} from "react";
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 import UserService from "../services/userService";
-import AuthService from "../services/authService";
-
-
 
 export default class User extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            content: ""
+            content: []
         };
     }
 
     componentDidMount() {
+        //find all users
         UserService.getUsers().then(
             response => {
+                //console.log(response)
                 this.setState({
                     content: response
                 });
@@ -32,12 +33,18 @@ export default class User extends Component {
     }
 
     render() {
+        const {content} = this.state;
+
         return (
             <div className="col-md-12">
-                <div>User page</div>
-                <p>{this.state.content}</p>
+                <BootstrapTable data={content} pagination striped hover>
+                    <TableHeaderColumn isKey dataField='userName'>Username</TableHeaderColumn>
+                    <TableHeaderColumn dataField='name'>Name</TableHeaderColumn>
+                    <TableHeaderColumn dataField='lastName'>Last Name</TableHeaderColumn>
+                    <TableHeaderColumn dataField='email'>Email</TableHeaderColumn>
+                    <TableHeaderColumn dataField='createdAt'>Create Date</TableHeaderColumn>
+                </BootstrapTable>
             </div>
-
         )
     }
 }
